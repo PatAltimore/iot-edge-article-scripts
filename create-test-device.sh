1a430d87fb4e70a3abec8103b649edf8143604fa
Pat Altimore <17440249+PatAltimore@users.noreply.github.com>
2022-10-27 12:50:25 -0700

    Add create test device script

---
+++ create-test-device.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Usage: create-test-device.sh <IoT device ID name>
+# Use defaults to create test device
+
+# Set up variables based on arguments or defaults
+RESOURCE_GROUP="IoTEdgeResources"
+IOT_HUB="patricka-iot-hub"
+IOT_DEVICE="${1:-patricka-device1}" # First argument or sample value patricka-iotedge-device
+VM_NAME= IOT_DEVICE + "-vm"
+KEYVAULT_NAME="patricka-keyvault"
+
+# Call scripts to create resources
+source create-iotedge-device.sh $RESOURCE_GROUP $IOT_HUB $IOT_DEVICE
+source deploy-iotedge-linux-device-ssh.sh $RESOURCE_GROUP $IOT_HUB $IOT_DEVICE $VM_NAME $KEYVAULT_NAME
